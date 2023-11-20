82a4ec0b19b654adc741e259e4f05e5eda4ddbd7
Manny <tristan.michel@epitech.eu>
2023-11-20 16:27:37 +0100

    portfolio 2023

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
 import Loader from "../components/Loader"
 
 import Island from "../models/Island"
+import Sky from "../models/Sky"
+import Bird from "../models/Bird"
+import Plane from "../models/Plane"
+
         {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
             POPUP
         </div>  */}
 
 const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
@@ -30,16 +36,18 @@ const Home = () => {
         <Canvas className="w-ful h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
             <Suspense fallback={<Loader />}>
                 <directionalLight position={[1, 1, 1]} intensity={2} />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <ambientLight intensity={0.5} />
+                <hemisphereLight skyColor="#b1e1FF" groundColor="#000000" intensity={1}/>
                 
+
+                <Bird />
+                <Sky />
                 <Island 
                     position={islandPosition}
                     scale={islandScale}
                     rotation={islandRotation}
                 />
+                <Plane />
             </Suspense>
         </Canvas>
     </section>
