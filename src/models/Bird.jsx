e531abb12725e6701ece7ee71f47c21c58d1cc55
Ph4nToM00 <tristan.michel@epitech.eu>
2023-11-20 20:40:28 +0100

    portfolio 2023

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import { useGLTF, useAnimations } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 import birdScene from '../assets/3d/bird.glb';
 
@@ -12,7 +13,26 @@ const Bird = () => {
 
     useEffect(() => {
       actions['Take 001'].play();
-    }, [])
+    }, []);
+
+    useFrame(({ clock, camera}) => {
+      /* birdRef.current.rotation.y += 0.15 * delta; */
+      birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+      if(birdRef.current.position.x > camera.position.x + 10) {
+        birdRef.current.rotation.y = Math.PI;
+      } else if(birdRef.current.position.x < camera.position.x - 10) {
+        birdRef.current.rotation.y = 0;
+      }
+
+      if(birdRef.current.rotation.y === 0) {
+        birdRef.current.position.x += 0.01;
+        birdRef.current.position.z -= 0.01;
+      } else {
+        birdRef.current.position.x -= 0.01;
+        birdRef.current.position.z += 0.01;
+      }
+    }, []);
 
   return (
     <mesh position={[-5, 2, 1]}
