1640ce18ca92a02c1f6944110074166a6f551b17
Manny <tristan.michel@epitech.eu>
2023-11-21 15:09:06 +0100

    portfolio 2023

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,66 @@
-import React, { useState } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
 
-
+import Loader from '../components/Loader';
+import Fox from '../models/Fox';
 
 const Contact = () => {
 
+  const formRef = useRef(null);
+
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
 
-  const handleChange = () => {};
-  const handleFocus = () => {};
-  const handleBlur = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation('hit');
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Tristan",
+        from_email: form.email,
+        to_email: "tristanmichelpro@gmail.com",
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      // TODO: Notification d'envoie
+      // TODO: Cacher l'alerte
+
+      setForm({ name: '', email: '', message: '' })
+    }).catch((error) => {
+      setIsLoading(false);
+      setCurrentAnimation('idle');
+      console.log(error);
+      // TODO: Montrer l'erreur
+    })
+  };
+
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');
+
+
+ 
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Prenez contact avec moi</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
           <label className="text-balck-500 font-semibold">Nom
           <input type="text" name="name" className="input" placeholder="John Doe" required 
             value={form.name} 
@@ -44,6 +88,28 @@ const Contact = () => {
           </label>
           <button type='submit' className="btn" disabled={isLoading} onFocus={handleFocus} onBlur={handleBlur}>{isLoading ? 'Envoi en cours ...' : 'Envoyer votre message'}</button>
         </form>
+      </div>
+
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox 
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
