bbac338730900ccf1d218d522b7f25b35fa69a64
Manny <tristan.michel@epitech.eu>
2023-11-21 16:57:10 +0100

    portfolio 2023

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,23 @@ const About = () => {
   return (
     <section className="max-container">
       <h1 className="head-text">
-        Bonjour, Je suis <span className="blue-gradient_text">Tristan</span>
+        Bonjour, Je suis <span className="blue-gradient_text font-semibold drop-shadow">Tristan</span>
       </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+        Je suis développeur Fullstack vivant en France, passionné par l'informatique. <br />
+        Mon intérêt s'étend aussi bien au développement de sites web qu'à la création d'applications mobiles.
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">Mes compétences</h3>
+
+        <div className="mt-16 flex flex-wrap gap-12">
+          
+        </div>
+      </div>
     </section>
   )
 }
